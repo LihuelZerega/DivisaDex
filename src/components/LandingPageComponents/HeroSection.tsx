28631c6b5c131cd332f7619e3bf2a7b3aabb172d
lihuel <zeregalihuel@gmail.com>
2023-10-23 18:52:17 -0300

    HeroSection Fix

--- src/components/LandingPageComponents/HeroSection.tsx
+++ src/components/LandingPageComponents/HeroSection.tsx
@@ -6,7 +6,7 @@ const HeroSection = () => {
             <h1 className="text-5xl font-bold mb-4">Divisa Dex</h1>
             <h2 className="text-4xl mb-6">Your view of global value</h2>
             <div className="flex space-x-4">
-                <button className="bg-teal-light py-2 px-6 rounded-lg text-teal-dark font-semibold">Join Nw</button>
+                <button className="bg-teal-light py-2 px-6 rounded-lg text-teal-dark font-semibold">Join Now</button>
             </div>
         </div>
     )
